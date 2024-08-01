5038d3a621386b36e98fcb43c2a0d38427e752e1
Guy Tavor <guy@scoompa.com>
2024-08-01 09:31:43 +0300

    fix dockerfile to work on gcp

--- upload_docker_image.sh
+++ upload_docker_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=~/secrets/jukee-1-f9bd1225c645.json
-docker compose up
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+docker compose build
 gcloud auth login
 gcloud config set project jukee-1
 docker tag gcui-suno-api-suno-api:latest gcr.io/jukee-1/suno_api:v1.0
